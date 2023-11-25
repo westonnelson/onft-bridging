f8a873a767a55b1973219fd9273658bb613804bb
westonnelson <dogooddowell@protonmail.com>
2023-11-25 17:47:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ deploy/LilPudgysONFT.js
@@ -0,0 +1,43 @@
+const LZ_ENDPOINTS = require("../constants/layerzeroEndpoints.json")
+const CHAIN_IDS = require("../constants/chainIds.json");
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+    console.log(`>>> your address: ${deployer}`)
+
+    const lzEndpointAddress = LZ_ENDPOINTS[hre.network.name]
+    console.log(`[${hre.network.name}] Endpoint Address: ${lzEndpointAddress}`)
+
+    await deploy("LilPudgysONFT", {
+        from: deployer,
+        args: ["https://api.pudgypenguins.io/lil/", "LilPudgys", "LP", 150000, lzEndpointAddress], // mainnet
+        // args: ["https://api.pudgypenguins.io/lil/", "LilPudgys", "LP", 100000, lzEndpointAddress], // testnet
+        log: true,
+        waitConfirmations: 3,
+        skipIfAlreadyDeployed: true
+    })
+
+    let onft = await ethers.getContract("LilPudgysONFT")
+
+    let enabledChains = ["ethereum", "bsc", "arbitrum", "polygon"] // mainnet
+    // let enabledChains = ["ethereum-goerli", "arbitrum-goerli"] // testnet
+
+    if (enabledChains.includes(hre.network.name)) {
+        if (hre.network.name == "arbitrum" || hre.network.name == "arbitrum-goerli") {
+            await(await onft.setMinGasToTransferAndStore(500000)).wait()
+        }
+
+        for (let n of enabledChains) {
+            if (n != hre.network.name) {
+                await(await onft.setDstChainIdToTransferGas(CHAIN_IDS[n], 50000)).wait()
+                await(await onft.setDstChainIdToBatchLimit(CHAIN_IDS[n], 25)).wait()
+                await(await onft.setMinDstGas(CHAIN_IDS[n], 1, 150000)).wait()
+            }
+        }
+    }
+
+    await hre.run("verifyContract", { contract: "LilPudgysONFT" })
+}
+
+module.exports.tags = ["LilPudgysONFT"]
---
+++ tasks/index.js
@@ -0,0 +1,20 @@
+task("getSigners", "show the signers of the current mnemonic", require("./getSigners")).addOptionalParam("n", "how many to show", 3, types.int)
+
+task(
+    "setTrustedRemote",
+    "setTrustedRemote(chainId, sourceAddr) to enable inbound/outbound messages with your other contracts",
+    require("./setTrustedRemote")
+).addParam("targetNetwork", "the target network to set as a trusted remote")
+    .addOptionalParam("localContract", "Name of local contract if the names are different")
+    .addOptionalParam("remoteContract", "Name of remote contract if the names are different")
+    .addOptionalParam("contract", "If both contracts are the same name")
+
+task("sendToken", "send an ONFT nftId from one chain to another", require("./sendToken"))
+    .addParam("targetNetwork", "the chainId to transfer to")
+
+task("clearCredits", "clear credits", require("./clearCredits"))
+
+task("verifyContract", "", require("./verifyContract.js"))
+    .addParam("contract", "contract name")
+
+task("transferOwnership", "", require("./transferOwnership.js"))
